2238eda2a8090f7518fea79f2a373112bb928afe
Maggie Oudsema <maggieoudsema@gmail.com>
2021-02-04 18:55:47 -0500

    added break and conitnue examples

--- 2-3JS.js
+++ 2-3JS.js
@@ -12,9 +12,27 @@
 // };
 
 // do...while loops
-let a = 0;
-do {
-    console.log (a);
-    a++;
-} while (a < 20);
+// let a = 0;
+// do {
+//     console.log (a);
+//     a++;
+// } while (a < 20);
 
+
+// using break to stop the loop at 5 even though it goes to 10
+// let a = 0
+// for (let a = 0; a < 10; a++) {
+//     console.log (a);
+//     if (a === 5) {
+//         break;
+//     }
+// }
+
+// using continue and console log afterwards skips 5
+let a = 0
+for (let a = 0; a < 10; a++) {
+    if (a === 5) {
+        continue;
+    }
+    console.log (a);
+}
