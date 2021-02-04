7851963d7e5efe261c9dfce7245ffe4eb99a5850
Maggie Oudsema <maggieoudsema@gmail.com>
2021-02-03 23:16:55 -0500

    first commit

---
+++ 2-3JS.js
@@ -0,0 +1,19 @@
+// while loop
+// let a = 0;
+// while (a < 20) {
+//     console.log (a);
+//     a++;
+// }
+// if console log is after a++ would go show 1-19, where as written above it shows 0-19.
+
+// for loop
+// for (let a = 0; a < 20; a++) {
+//     console.log (a);
+// };
+
+// do...while loops
+let a = 0;
+do {
+    console.log (a);
+    a++;
+} while (a < 20);
---
+++ string.js
@@ -0,0 +1,123 @@
+const prompt = require('prompt-sync')();
+var colors = require('colors');
+
+
+// const firstName = prompt ("What is your first name? ");
+
+// // console.log (`Hello ${firstName}!`);
+
+// const lastName = prompt ("What is your last name? ");
+
+// console.log (firstName + " " + lastName);
+// console.log (`Hello ${firstName} ${lastName}`);
+
+// const user = {
+//     firstName: "Maggie",
+//     lastName: "Oudsema",
+//     age: 34,
+//     pet1: { 
+//         petName: "Basil",
+//         type: "dog",
+//     },
+//     pet2: {
+//         petName: "Lennon",
+//         type: "gecko",
+//     },
+//     pet3: {
+//         petName: "Taboo",
+//         type: "fish",
+//     }
+// };
+
+// console.log (user)
+
+// need line one uncommented for this block
+
+// const eMail = prompt ("Email LogIn ");
+// const password = prompt ("Password? ")
+
+// if (eMail === 'maggieoudsema@gmail.com' && password === 'Password!') {
+//     console.log ("Access granted")
+// } else {
+//     console.log ("Try again...")
+// }
+
+// switch statements
+
+let pizza = null; 
+let extraCheese = null; 
+let extraToppings = null;
+let another = null;
+let toppings = null;
+let total = 0;
+// const taxes = 0.06;
+
+do {
+    do {
+        pizza = prompt ("What size pizza do you want? (S, M, L, XL) ".green).toUpperCase();
+        switch (pizza) {
+            case  "S":
+                total += 5;
+                break;
+            case "M":
+                total += 6;
+                break;
+            case "L":
+                total += 7;
+                break;
+            case "XL":
+                total += 8;
+                break;
+            default:
+                console.log("Invalid entry please try again.".red);
+        };
+        if(pizza === 'S' || pizza === 'M'|| pizza === 'XL' || pizza === 'XL'){
+            pizza = 1;
+            }
+    }while(pizza !== 1);
+    
+    extraCheese = prompt ("Do you want extra cheese? (Y/N) ".yellow).toUpperCase();
+
+    // terinary operator option
+    total += extraCheese === "Y" ? 1 : 0;
+
+    do{
+        extraToppings = prompt ("Do you want any other toppings? (Y/N) ".grey).toUpperCase();
+        if (extraToppings === "Y") {
+            toppings = prompt ("Please select: mushroom (M), onion (O), green pepper (G), pepperoni (P), sausage (S), or bacon (B)? ".grey).toUpperCase();
+            
+            switch (toppings) {
+            case  "M":
+                total += 2;
+                break;
+            case "O":
+                total += 2;
+                break;
+            case "G":
+                total += 2; 
+                break;   
+            case "P":
+                total += 3;
+                break;
+            case "S":
+                total += 3;
+                break;
+            case "B":
+                total += 5;
+                break;
+            };
+        }; 
+    }while (extraToppings === "Y");
+    
+    another = prompt ("Would you like another pizza? (Y/N) ".grey).toUpperCase();
+} while (another === "Y");
+
+console.log (`Your total cost is $${total}.00`.rainbow);
+
+
+// if/else statement
+// if (extraCheese === "Y") {
+//     total = total+1
+// } else {
+//     total = total+0
+// };
